d897dd3ed0845f25a030e33ea15d45b080910274
j1ai <laijustinholam@gmail.com>
2019-10-27 03:07:16 -0400

    Create react and django template

--- app/backend/backend/urls.py
+++ app/backend/backend/urls.py
@@ -1,4 +1,4 @@
-"""narwhals URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('narwhals.urls')),
 ]
