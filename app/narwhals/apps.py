31e96abdfbbf0f4eb4c9fed74117c7cf94d163c1
j1ai <laijustinholam@gmail.com>
2019-11-28 13:29:21 -0500

    deployment fix

--- app/narwhals/apps.py
+++ app/narwhals/apps.py
@@ -8,7 +8,7 @@ from .metastaticPredictor import MetastaticPredictor
 class NarwhalsConfig(AppConfig):
     name = 'narwhals'
     MODEL_PATH = pathlib.Path("./ml_model")
-    METASTATIC_AI_MODEL_PATH = "ml_model/final_model.h5"
-    METASTATIC_AI_MODEL_WEIGHTS_PATH = "ml_model/final_model_weights.h5"
-    ML_PREDICTION_PROCESSING_PATH = pathlib.Path("ml_prediction_processing")
+    METASTATIC_AI_MODEL_PATH = "./ml_model/final_model.h5"
+    METASTATIC_AI_MODEL_WEIGHTS_PATH = "./ml_model/final_model_weights.h5"
+    ML_PREDICTION_PROCESSING_PATH = pathlib.Path("./ml_prediction_processing")
     predictor = MetastaticPredictor(METASTATIC_AI_MODEL_PATH, METASTATIC_AI_MODEL_WEIGHTS_PATH, ML_PREDICTION_PROCESSING_PATH)
